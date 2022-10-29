e42ee453f9b5e61b5a4e6563e880bed82886d313
RoXoRkA26 <roxor.zelezny@gmail.com>
2022-10-29 13:54:52 +0200

    Created Header files for sensors

---
+++ sensors/Inc/hts221.h
@@ -0,0 +1,13 @@
+/*
+ * hts221.h
+ *
+ *  Created on: 29. 10. 2022
+ *      Author: jakub
+ */
+
+#ifndef INC_HTS221_H_
+#define INC_HTS221_H_
+
+
+
+#endif /* INC_HTS221_H_ */
---
+++ sensors/Inc/lps25hb.h
@@ -0,0 +1,13 @@
+/*
+ * lps25hb.h
+ *
+ *  Created on: 29. 10. 2022
+ *      Author: jakub
+ */
+
+#ifndef INC_LPS25HB_H_
+#define INC_LPS25HB_H_
+
+
+
+#endif /* INC_LPS25HB_H_ */
